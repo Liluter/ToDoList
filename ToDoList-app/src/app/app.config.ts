9b893279f7f27f609b580a78d6e1db680b6577aa
Karol <karolawdziewicz@gmail.com>
2024-06-08 12:01:26 +0200

    After firebase init GH actions

--- ToDoList-app/src/app/app.config.ts
+++ ToDoList-app/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"todo-list-lowgular","appId":"1:233511898140:web:d3655248dff6cf6c7b23d9","storageBucket":"todo-list-lowgular.appspot.com","apiKey":"AIzaSyApSofMDhSd_yU1DbuPVVZy3p4XCNvVY1U","authDomain":"todo-list-lowgular.firebaseapp.com","messagingSenderId":"233511898140"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService]
 };
