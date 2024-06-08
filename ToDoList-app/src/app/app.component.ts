6885dc2e04fdaebd3dce13687df6abe7d981bd55
Karol <karolawdziewicz@gmail.com>
2024-06-08 23:32:32 +0200

    Configure Rest Api call

--- ToDoList-app/src/app/app.component.ts
+++ ToDoList-app/src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoistApi, Task } from "@doist/todoist-api-typescript"
+import { environment } from './env'
+import { CommonModule } from '@angular/common';
+import { Observable, from, tap, debounceTime, switchMap, distinctUntilChanged } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ToDoList-app';
+export class AppComponent implements OnInit {
+  clickEvent = new EventEmitter<void>()
+  destroyRef = inject(DestroyRef)
+  clickObservable$: Observable<void>
+  tasks$!: Observable<Task[]>
+  api: TodoistApi
+  constructor() {
+    this.api = new TodoistApi(environment.restApitoken)
+    this.clickObservable$ = from(this.clickEvent)
+
+  }
+
+  ngOnInit() {
+    this.clickObservable$.pipe(
+      debounceTime(1000),
+      switchMap(() => this.tasks$ = from(this.api.getTasks())),
+      tap((data) => console.log(data)),
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe()
+  }
+  getTasks() {
+    this.clickEvent.emit();
+  }
 }
+
+
+
---
+++ ToDoList-app/src/app/env.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  restApitoken: 'a06b19d79d9d93fe4467b74fa167b2872d29233a'
+}
