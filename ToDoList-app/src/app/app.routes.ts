ba1ade448d6c6ba436e466ec082e95819e5dfd02
Karol <karolawdziewicz@gmail.com>
2024-07-14 20:40:06 +0200

    Add tasks path, and tasks page compoent.

--- ToDoList-app/src/app/app.routes.ts
+++ ToDoList-app/src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome/welcome-page.component';
 import { ProjectsPageComponent } from './pages/projects/projects-page.component';
 import { LabelsPageCompoent } from './pages/labels/labels-page.component';
+import { TasksPageComponent } from './pages/tasks/tasks-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'welcome',
     pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksPageComponent
   },
   {
     path: 'projects',
---
+++ ToDoList-app/src/app/pages/tasks/tasks-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: '',
+  standalone: true
+})
+export class TasksPageComponent {
+
+}
