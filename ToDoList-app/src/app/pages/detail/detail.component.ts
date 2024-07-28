089f7000cf55b950a1013becf7be262fbdae5771
Karol <karolawdziewicz@gmail.com>
2024-07-28 22:54:21 +0200

    Create detail component , add services.

--- ToDoList-app/src/app/pages/detail/detail.component.ts
+++ ToDoList-app/src/app/pages/detail/detail.component.ts
@@ -1,9 +1,53 @@
-import { Component, Input } from "@angular/core";
-
+import { Component, inject, Input, OnInit } from "@angular/core";
+import { ApiCallsService } from "../../services/api-calls.service";
+import { map, Observable, switchMap, tap } from "rxjs";
+import { Item } from "../../interfaces/item.interface";
+import { AsyncPipe, DatePipe, JsonPipe, NgClass } from "@angular/common";
+import { badgeClass, getLabelColor, getProjectColor } from "../../utilities/utility";
+import { RouterModule } from "@angular/router";
+import { Label } from "../../interfaces/label.interface";
+import { SyncProject } from "../../interfaces/syncProject.interface";
+import { SyncProjects } from "../../interfaces/syncProjects.interface";
 @Component({
   templateUrl: './detail.component.html',
-  standalone: true
+  standalone: true,
+  imports: [AsyncPipe, JsonPipe, NgClass, DatePipe, RouterModule]
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  getProjectColor = getProjectColor
+  badgeClass = badgeClass
+  getLabelColor = getLabelColor
   @Input() id?: string
+  apiService: ApiCallsService = inject(ApiCallsService)
+  task$?: Observable<Item>
+  descriptionOpenHandler: string | undefined;
+  allLabels$: Observable<Label[]> = this.apiService.getAllLabels()
+  projectId?: string
+  project$?: Observable<SyncProject>;
+  projects?: SyncProject[]
+  allProjects$: Observable<SyncProject[]> = this.apiService.getAllProjects().pipe(map(data => data.projects))
+  ngOnInit() {
+
+    if (this.id) {
+      this.task$ = this.apiService.getTaskById(this.id).pipe(
+        tap(data => {
+          console.log(data.item.project_id)
+          this.getProject(data.item.project_id)
+        }),
+        map(data => data.item)
+      )
+    }
+  }
+  openDescription(id: string | undefined) {
+    if (this.descriptionOpenHandler === id) {
+      this.descriptionOpenHandler = ''
+      return
+    }
+    this.descriptionOpenHandler = id;
+  }
+  getProject(id: string) {
+    this.project$ = this.apiService.getProjectById(id).pipe(
+      map(data => data.project)
+    )
+  }
 }
--- ToDoList-app/src/app/services/api-calls.service.ts
+++ ToDoList-app/src/app/services/api-calls.service.ts
@@ -2,8 +2,8 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Label } from '../interfaces/label.interface';
 import { environment } from '../varibles/env';
-import { Observable } from 'rxjs';
-import { completedUrl, labelsUrl, projectsUrl, syncUrl, tasksUrl } from '../varibles/urls';
+import { EMPTY, map, Observable, switchMap } from 'rxjs';
+import { completedUrl, labelsUrl, projectsUrl, syncUrl, tasksUrl, itemUrl, getProjectUrl } from '../varibles/urls';
 import { SyncItem } from '../interfaces/syncItem.interface';
 import { AllCompleted } from '../interfaces/all-completed.interface';
 import { SyncProjects } from '../interfaces/syncProjects.interface';
@@ -11,6 +11,10 @@ import { Project } from '../interfaces/project.interface';
 import { Task } from '../interfaces/task.interface';
 import { SimpleLabel } from '../interfaces/simpleLabel.interface';
 import { AddType, Modals, TasksType } from '../types/modals';
+import { Item } from '../interfaces/item.interface';
+import { GetItem } from '../interfaces/getItem.interface';
+import { SyncProject } from '../interfaces/syncProject.interface';
+import { GetSyncProject } from '../interfaces/getSyncProject.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -19,6 +23,7 @@ export class ApiCallsService {
     'Authorization': 'Bearer ' + environment.restApitoken
   })
   private itemsParams = new HttpParams({ fromObject: { sync_token: '*', resource_types: '["items"]' } })
+
   private projectsParams = new HttpParams({ fromObject: { sync_token: '*', resource_types: '["projects"]' } })
 
   tasksEvent = new EventEmitter<TasksType>()
@@ -72,5 +77,29 @@ export class ApiCallsService {
     return this.http.post(tasksUrl, data, {
       headers: this.authorization
     })
+  }
+  getTaskById(id: string): Observable<GetItem> {
+    if (id) {
+      const itemParams = new HttpParams({ fromObject: { all_data: 'false', item_id: id } })
+      return this.http.get<GetItem>(itemUrl,
+        {
+          headers: this.authorization,
+          params: itemParams
+        })
+    } else {
+      return EMPTY
+    }
+  }
+  getProjectById(id?: string) {
+    if (id) {
+      const itemParams = new HttpParams({ fromObject: { all_data: 'false', project_id: id } })
+      return this.http.get<GetSyncProject>(getProjectUrl,
+        {
+          headers: this.authorization,
+          params: itemParams
+        })
+    } else {
+      return EMPTY
+    }
   }
 }
