a20535ce9652f8a2538af5d2bd608393d2a972e7
Karol <karolawdziewicz@gmail.com>
2024-10-13 20:42:32 +0200

    Refactor add task component.

--- ToDoList-app/src/app/pages/add/task/add-task-page.component.ts
+++ ToDoList-app/src/app/pages/add/task/add-task-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, DestroyRef, EventEmitter, inject } from "@angular/core";
-import { ApiCallsService } from "../../../services/api-calls.service";
-import { map, Observable, shareReplay, tap } from "rxjs";
-import { SyncProject } from "../../../interfaces/syncProject.interface";
-import { FormsModule, NgForm } from "@angular/forms";
-import { Task } from "../../../interfaces/task.interface";
-import { task } from "../../../varibles/env";
-import { Label } from "../../../interfaces/label.interface";
+import { Component, inject, signal, Signal } from "@angular/core";
 import { AsyncPipe, DatePipe, NgClass } from "@angular/common";
-import { badgeClass, getLabelColor } from "../../../utilities/utility";
-import { ShowMessageService } from "../../../services/showMessage.service";
+import { FormsModule, NgForm } from "@angular/forms";
+import { toSignal } from "@angular/core/rxjs-interop";
+
+import { map, tap } from "rxjs";
+
+import { SyncProject } from "../../../interfaces/syncProject.interface";
+import { Label } from "../../../interfaces/label.interface";
+import { Task } from "../../../interfaces/task.interface";
 import { Message, MessageStatus } from "../../../types/message.interface";
+
+import { badgeClass, getLabelColor } from "../../../utilities/utility";
+import { task } from "../../../varibles/env";
+
+import { ApiCallsService } from "../../../services/api-calls.service";
+import { ShowMessageService } from "../../../services/showMessage.service";
+
 @Component({
   templateUrl: './add-task-page.component.html',
   styleUrl: './add-task-page.component.scss',
@@ -17,44 +23,31 @@ import { Message, MessageStatus } from "../../../types/message.interface";
   imports: [FormsModule, DatePipe, NgClass, AsyncPipe]
 })
 export class AddTaskPageComponent {
-  allProjects$?: Observable<[SyncProject]>
-  loadingState: boolean = false
-  newTask: Task = { ...task }
+  api: ApiCallsService = inject(ApiCallsService)
+  showMessageService: ShowMessageService = inject(ShowMessageService)
+  allProjects: Signal<[SyncProject] | null> = toSignal(this.api.getAllProjects().pipe(map(data => data.projects)), { initialValue: null })
+  allLabelsSignal: Signal<Label[]> = toSignal(this.api.getAllLabels().pipe(tap(labels => this.labels = labels.map(label => label.name))), { initialValue: [] })
+
+  loadingState = signal(false)
   readonly defaultTaskValue: Task = { ...task }
-  message?: string;
-  showCompletionMessage: boolean = false
-  completionSuccess?: boolean;
-  destroyRef = inject(DestroyRef)
-  allLabels$?: Observable<Label[]>;
-  menuObservable$: any;
+  newTask: Task = { ...task }
   currentDate: string = new Date().toISOString()
   badgeClass = badgeClass
   getLabelColor = getLabelColor
-  showMessageService: ShowMessageService = inject(ShowMessageService)
   labels: string[] = []
-  constructor(private readonly api: ApiCallsService) {
-
-    this.allLabels$ = this.api.getAllLabels().pipe(
-      tap(labels => this.labels = labels.map(label => label.name)))
-
-    this.allProjects$ = this.api.allProjects$?.pipe(
-      map(data => data.projects),
-      shareReplay()
-    )
-  }
 
   onAddTask(form: NgForm) {
-    this.loadingState = true
+    this.loadingState.set(true)
     let newTask = { ...this.newTask }
     newTask.labels = this.convertLabelBoolToStr()
     this.api.postTask(newTask).subscribe(data => {
       if (data) {
-        this.loadingState = false
+        this.loadingState.set(false)
         this.showMessage({ type: MessageStatus.success, text: `Task "${form.form.controls['title'].value}"  added successfully` })
         this.resetTask(form)
       }
     }, error => {
-      this.loadingState = false
+      this.loadingState.set(false)
       let message = error.message
       if (error.status === 403 || error.status === 400) {
         message = error.error
